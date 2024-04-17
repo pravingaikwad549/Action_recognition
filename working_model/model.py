bdebb0536d35e7431fb960aa2ab8b802e84b8cb1
Pravin <pravingaikwad549@gmail.com>
2024-04-17 13:37:23 +0530

    Modified model.py and train.py to get them ready for training on actual data

--- working_model/model.py
+++ working_model/model.py
@@ -1,17 +1,34 @@
 import torch
 from torch import nn
+from torch.nn import init
 
 class LSTMModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers, seq_length = 100):
         super(LSTMModel, self).__init__()
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
         self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.fc1 = nn.Linear(hidden_dim * seq_length, output_dim)
+        self.fc2 = nn.Linear(output_dim, output_dim)
+        self.init_weights()
+        self.softmax = nn.Softmax(dim=1)
+        self.relu = nn.ReLU()
+
+    def init_weights(self):    
+        for name, param in self.named_parameters():
+            if 'weight' in name:
+                if 'lstm' in name:
+                    init.kaiming_uniform_(param, mode='fan_in', nonlinearity='relu')
+                elif 'fc' in name:
+                    init.kaiming_uniform_(param, mode='fan_in', nonlinearity='linear')
 
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim)
         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim)
         out, _ = self.lstm(x, (h0, c0))
-        out = self.fc(out[:, -1, :])
+        out = self.fc1(out.flatten(start_dim=1))
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.softmax(out)
+        # print("After softmax",out)
         return out
--- working_model/train.py
+++ working_model/train.py
@@ -9,33 +9,47 @@ from torch.utils.tensorboard import SummaryWriter
 
 
 # Load data
-parent_folder_path = r"/home/pravin/Desktop/rough/fanply/UCF101/UCF-101"
-dataset, labels = load_data(parent_folder_path)
-
-# Set model parameters
-input_dim = 99
-hidden_dim = 128
-output_dim = 4
-num_layers = 2
-num_class = 4
-
-# Initialize model, criterion, and optimizer
-model = LSTMModel(input_dim, hidden_dim, output_dim, num_layers)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
+parent_folder_path = r"/home/pravin/Desktop/rough/fanply/UCF_EXP"
+dataset, labels, dict_lables = load_data(parent_folder_path)
 
 log_dir = "/home/pravin/Desktop/rough/fanply/runs/logs_1"  # Directory to save the logs
 writer = SummaryWriter(log_dir=log_dir)
 
 
+print("Label: ", labels.shape)
+print("Dataset: ", dataset.shape)
+print("Dict Labels: ", len(dict_lables))
+# Set model parameters
+input_dim = dataset.shape[2]
+hidden_dim = 128
+output_dim = len(dict_lables)
+num_layers = 2
+num_class = len(dict_lables)
+seq_length = dataset.shape[1]
+
+learning_rate = 1e-5
+
+print("Input Dim: ", input_dim)
+print("Output Dim: ", output_dim)
+print("Hidden Dim: ", hidden_dim)
+print("Num Layers: ", num_layers)
+print("Num Classes: ", num_class)
+print("Seq Length: ", seq_length)
+
+# Initialize model, criterion, and optimizer
+model = LSTMModel(input_dim, hidden_dim, output_dim, num_layers, seq_length=seq_length)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+
 # Train the model
-num_epochs = 10
-batch_size = 2
+num_epochs = 100
+batch_size = 32
 
 train_dataset = TensorDataset(dataset, labels)
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+train_loader = DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle=True)
 
 for epoch in tqdm(range(num_epochs)):
+    epoch_loss = 0.0
     for batch_input, batch_labels in train_loader:
         optimizer.zero_grad()
         output = model(batch_input)
@@ -43,7 +57,12 @@ for epoch in tqdm(range(num_epochs)):
         loss = criterion(output, batch_labels)
         loss.backward()
         optimizer.step()
-        writer.add_scalar("Loss/train", loss.item(), epoch)
+        epoch_loss += loss.item() 
+        writer.add_scalar('Loss/train', loss.item(), global_step=epoch)
     print(loss.item())
-print("Training complete!")
 writer.close()
+print("Training complete!")
+
+# Save the model
+torch.save(model.state_dict(), "model_action_recognition.pth")
+print("Model saved!")
